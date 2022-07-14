8743fdd034606944757333b22b7d87c9dfa37c11
yuanli <yuanli@lizhi.fm>
2022-07-14 16:25:41 +0800

    feat: prettier reformat.

--- components/Buidl.js
+++ components/Buidl.js
@@ -1,15 +1,10 @@
 import { Box, Link } from "@mui/material";
 import React from "react";
-import Summary from "./Summary";
 
 function Buidl() {
   return (<Box
     style={{
-      height: "198px",
-      background: "#F9F9F9",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
+      height: "198px", background: "#F9F9F9", display: "flex", alignItems: "center", justifyContent: "center"
     }}
   >
     <Link href={`https://lxdao.io/`} target="_blank" color={"inherit"}>
